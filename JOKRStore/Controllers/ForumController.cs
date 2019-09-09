4e6e3f3bab8f32d555add28f004f0c981a4970d8
SzJoakim93 <szabo.joakim@gmail.com>
2019-09-09 20:39:58 +0200

    Continue implementing forum. Create not working yet.

--- JOKRStore/Controllers/ForumController.cs
+++ JOKRStore/Controllers/ForumController.cs
@@ -1,11 +1,14 @@
 using JOKRStore.Web.Models;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
 using System.Diagnostics;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using BLL.ServiceInterfaces;
 using JOKRStore.Web.ViewModels;
 using AutoMapper;
+using System.Security.Claims;
 
 namespace JOKRStore.Web.Controllers
 {
@@ -23,6 +26,38 @@ namespace JOKRStore.Web.Controllers
         {
             var categories = await forumService.GetForumCategories();
             return View(mapper.Map<IEnumerable<ForumCategoryViewModel>>(categories));
+        }
+
+        public async Task<ActionResult> ForumTopicList(Guid Id)
+        {
+            var category = await forumService.GetForumCategoryByIdAsync(Id);
+            return View(mapper.Map<ForumCategoryViewModel>(category));
+        }
+
+        public async Task<ActionResult> ForumTopic(Guid Id)
+        {
+            var topic = await forumService.GetForumTopicByIdAsync(Id);
+            return View(mapper.Map<ForumTopicViewModel>(topic));
+        }
+
+        public async Task<ActionResult> NewTopicForm(Guid Id)
+        {
+            ForumTopicViewModel new_model = new ForumTopicViewModel
+            {
+                ForumCategoryId = Id
+            };
+
+            return View("NewForum", new_model);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> NewTopic(ForumTopicViewModel new_model)
+        {
+            var UserId = User.Claims.Where(c => c.Type == ClaimTypes.NameIdentifier).First().Value;
+            await forumService.AddNewTopicAsync(Guid.Parse(UserId), new_model.ForumCategoryId, new_model.title, new_model.content);
+
+            return RedirectToAction("ForumTopicList", new_model.ForumCategoryId);
+            //return Content(CategoryId);
         }
     }
 }
